f9ce3dfe848c43c124c7e453e6ad2b85cd09ca21
matheusprado1 <matheusprado1096@gmail.com>
2023-01-08 14:19:36 -0300

    JavaScript

--- JavaScript/leap.js
+++ JavaScript/leap.js
@@ -1,3 +1,13 @@
+/**
+ * Returns a string indicating whether the given year is a leap year or not.
+ * 
+ * A year is a leap year if it is divisible by 4, unless it is also divisible by 100 but not by 400.
+ * 
+ * @param {number} year - The year to check.
+ * @return {string} - "Leap year." if the year is a leap year, "Not leap year." otherwise.
+ */
+
+
 function isLeap(year) {
     if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)){
         return "Leap year."
