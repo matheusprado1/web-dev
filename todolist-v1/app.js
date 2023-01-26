9940a54708dbf06355f474157d64ed6a656dd525
matheusprado1 <matheusprado1096@gmail.com>
2023-01-26 12:00:52 -0300

    to-do list v1 finished

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,43 +1,50 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const date = require(__dirname + '/date.js')
+
 
 const app = express()
 
+const items = ['Buy Food', 'Cook Food', 'Eat Food']
+const workItems = []
+
 app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
 
-  let today = new Date()
-  let currentDay = today.getDay()
-  let day = ''
+  const day = date.getDate()
 
-  switch (currentDay) {
-    case 0:
-      day = 'Sunday'
-      break
-    case 1:
-      day = 'Monday'
-      break
-    case 2:
-      day = 'Tuesday'
-      break
-    case 3:
-      day = 'Wednesday'
-      break
-    case 4:
-      day = 'Thursday'
-      break
-    case 5:
-      day = 'Fryday'
-      break
-    case 6:
-      day = 'Saturday'
-      break
-    default:
-      console.log('Error: current day is equal to: ' + currentDay)
+  res.render('list', {
+    listTitle: day,
+    newListItems: items
+  })
+
+})
+
+app.post('/', function (req, res) {
+
+  const item = req.body.newItem
+
+  if (req.body.list === 'Work') {
+    workItems.push(item)
+    res.redirect('/work')
+  } else {
+    items.push(item)
+    res.redirect('/')
   }
-  res.render('list', { kindOfDay: day })
 
+})
+
+app.get('/work', function (req, res) {
+  res.render('list', { listTitle: 'Work List', newListItems: workItems })
+})
+
+
+app.get('/about', function (req, res) {
+  res.render('about')
 })
 
 
